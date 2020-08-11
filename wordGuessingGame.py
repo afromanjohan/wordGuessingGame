9cd5302a94584b1f788422b68efafeb03a197ef6
afromanjohan <49200792+afromanjohan@users.noreply.github.com>
2020-08-11 21:30:11 +0200

    Update wordGuessingGame.py

--- wordGuessingGame.py
+++ wordGuessingGame.py
@@ -1,12 +1,18 @@
+# Import libraries
 from PyDictionary import PyDictionary
 dictionary = PyDictionary()
 from random_words import RandomWords
 r = RandomWords()
+
+# Select random word for the game
 word = r.random_word()
+
+# Create variables to keep track of game state
 count_guesses = 0
 previous_guesses = []
-print("Type a suggestion to get started: ")
 
+
+# Helping function to sort list of guesses and inform about the status of last guess
 def guess_is_before_solution(guess):
     list = [word]
     list.append(guess)
@@ -17,11 +23,13 @@ def guess_is_before_solution(guess):
     previous_guesses.append((guess, "before"))
     return guess + " is before solution"
 
-
+# Main game loop
+print("Type a suggestion to get started: ")
 while True:
     if len(previous_guesses) > 0:
         previous_guesses.sort()
-        print(previous_guesses)
+        for i in range(len((previous_guesses))):
+            print(previous_guesses[i])
     userSuggestion = input()
     if userSuggestion == "1":
         print("The word was: " + word)
@@ -33,5 +41,5 @@ while True:
         print("You won! The word was: " + str(word) + ", you got it after: " + str(count_guesses) + " amount of guesses.")
         break
     else:
-        print(guess_is_before_solution(userSuggestion))
         count_guesses += 1
+        print(guess_is_before_solution(userSuggestion) + ", guess number: " + str(count_guesses))
